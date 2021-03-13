a0041a85ace0bcd48b2b5435663d50eff9c49862
hakan çakmak <hakan.cakmak@live.com>
2021-03-13 15:47:16 +0300

    ortalama hesaplamak için fonksiyon yazıldı for döngüleri kullanıldı. data class oluşturuldu. dönüştürme metotdu eklendi. test edildi.

---
+++ app/src/main/java/com/badlogic/androidgames/ortalamabul/Dersler.kt
@@ -0,0 +1,5 @@
+package com.badlogic.androidgames.ortalamabul
+
+data class Dersler(var DersAdi:String, var DersKredi:String, var DersHarf:String) {
+    //mainde val değişkeninin altına arraylist ekle
+}
--- app/src/main/java/com/badlogic/androidgames/ortalamabul/MainActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/ortalamabul/MainActivity.kt
@@ -6,20 +6,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
-import kotlinx.android.synthetic.main.yeni_icerik_layout.*
 import kotlinx.android.synthetic.main.yeni_icerik_layout.view.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    private val Dersler : Array<String> = arrayOf("Java", "PHP", "Kotlin", "Unity", "Laravel", "Linux", "Mobil Platform" )
+    private val DERSLER : Array<String> = arrayOf("Java", "PHP", "Kotlin", "Unity", "Laravel", "Linux", "Mobil Platform" )
+    private var tumDersler:ArrayList<Dersler> = ArrayList(7)
+    // for döngüsüne geri dönelim en hesaplada.
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, Dersler)
+        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, DERSLER)
         tvDersler.setAdapter(adapter)
 
         if (rootLayout.childCount == 0) {
@@ -71,7 +72,31 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun ortalamaHesapla(view: View) {}
+    fun ortalamaHesapla(view: View) {
+        var toplamHarf:Double = 0.0
+        var toplamKredi:Double = 0.0
+        //şimdi harfleri double nota çevirecek bir metoda iht. var. dönüştürme metodu oluşturalım.
+
+        for(i in 0..rootLayout.childCount - 1) {
+            var birSatir = rootLayout.getChildAt(i)
+            //not ve kredileri tutacak bir data class oluşturalım
+            var geciciDers = Dersler(birSatir.tvYeniDersler.text.toString(),
+                ((birSatir.spYeniKredi.selectedItemPosition)+1).toString(),
+                birSatir.spYeniHarf.selectedItem.toString())
+
+            tumDersler.add(geciciDers)
+        }
+
+        for (mevcutDersler in tumDersler)
+        {
+            //metodun başına gelelim
+            toplamHarf += harfCevirNot(mevcutDersler.DersHarf) * mevcutDersler.DersKredi.toDouble()
+            toplamKredi += mevcutDersler.DersKredi.toDouble()
+        }
+
+        Toast.makeText(this, "Ortalamanız: "+toplamHarf/toplamKredi, Toast.LENGTH_LONG).show()
+        tumDersler.clear()
+    }
 
     fun spinnerIndexBul(spinner: Spinner, arananDeger: String): Int {
         var index = 0
@@ -88,5 +113,22 @@ class MainActivity : AppCompatActivity() {
         tvDersler.setText("")
         spHarf.setSelection(0)
         spKredi.setSelection(0)
+    }
+
+    fun harfCevirNot(gelenHarf:String) : Double {
+        var deger = 0.0
+        when(gelenHarf)
+        {
+            "AA" -> deger= 4.0
+            "BA" -> deger= 3.5
+            "BB" -> deger= 3.0
+            "CB" -> deger= 2.5
+            "CC" -> deger= 2.0
+            "DC" -> deger= 1.5
+            "DD" -> deger= 1.0
+            "FF" -> deger= 0.0
+        }
+        return deger
+        // 2. fora gidelim hesaplada.
     }
 }
