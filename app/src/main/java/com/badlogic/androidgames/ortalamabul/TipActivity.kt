eccc4b3e2e391e09b49fed70361ab96eca655bcc
hakan çakmak <hakan.cakmak@live.com>
2021-04-02 01:20:28 +0300

    tip activity yaratıldı. binding teknolojisi  module entegre edildi ve kullanıldı. formatlayıcı kullanıldı. string.xml e dahil edildi. Hesaplama yapıldı.

--- app/src/main/java/com/badlogic/androidgames/ortalamabul/TipActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/ortalamabul/TipActivity.kt
@@ -2,10 +2,43 @@ package com.badlogic.androidgames.ortalamabul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.badlogic.androidgames.ortalamabul.databinding.ActivityTipBinding
+import java.text.NumberFormat
 
 class TipActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityTipBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_tip)
+        binding = ActivityTipBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+            tipHesapla()
+        }
+    }
+
+    fun tipHesapla() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+        val tipPercentage = when(selectedId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+        var tip = tipPercentage * cost
+
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp) {
+            tip = kotlin.math.ceil(tip)
+        }
+
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+
     }
 }
