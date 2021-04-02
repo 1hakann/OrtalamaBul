289da11dfa39120244cc297f45ef6b557ed09a39
hakan çakmak <hakan.cakmak@live.com>
2021-04-03 02:06:38 +0300

    material desgin kullanıldı. Dark Mode çalışıldı. Uygulama içi icon kullanıldı.

--- app/src/main/java/com/badlogic/androidgames/ortalamabul/TipActivity.kt
+++ app/src/main/java/com/badlogic/androidgames/ortalamabul/TipActivity.kt
@@ -7,7 +7,7 @@ import java.text.NumberFormat
 
 class TipActivity : AppCompatActivity() {
 
-    lateinit var binding : ActivityTipBinding
+    private lateinit var binding : ActivityTipBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,12 +19,16 @@ class TipActivity : AppCompatActivity() {
         }
     }
 
-    fun tipHesapla() {
+    private fun tipHesapla() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
+        val cost = stringInTextField.toDoubleOrNull()
 
-        val tipPercentage = when(selectedId) {
+        if(cost == null || cost == 0.0) {
+            displayTip(0.0)
+            return
+        }
+
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -32,13 +36,17 @@ class TipActivity : AppCompatActivity() {
 
         var tip = tipPercentage * cost
 
-        val roundUp = binding.roundUpSwitch.isChecked
-        if(roundUp) {
+        if(binding.roundUpSwitch.isChecked) {
             tip = kotlin.math.ceil(tip)
+
         }
 
+       displayTip(tip)
+
+    }
+
+    private fun displayTip(tip : Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
-
     }
 }
